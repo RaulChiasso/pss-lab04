dce5733308624bbaa6c59cfb649cee2fd6d2cd1d
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-17 11:29:42 +0200

    Fix formatting

--- 02-qa/src/main/java/IT/UNIBO/qa/cat.java
+++ 02-qa/src/main/java/IT/UNIBO/qa/cat.java
@@ -1,77 +1,54 @@
-package IT.UNIBO.qa;
+package it.unibo.qa;
 
-public class cat {
-//this class describes a cat
+public class Cat {
 
+    private static int  NINE = 9;
+    static public  int numberoflives= NINE;
+
+    private String name, color, breed;
+    private int age;
+
+    public Cat(String meow, final String c, final int age, final String korat) {
+        this.name = meow;
+        this.color = c;
+        this.age = age;
+        this.breed = korat;
+    }
+
+    // This method is useful because it allows the cat to meow and introduce itself to the world in a single line of code.
+    public String meow(){
+        return "Meow, meow, I am " 
+                + this.name + 
+                ", hear me purr, In this feline world, I'm the star, that's for sure." + 
+                "With whiskers long and fur so sleek, I'm here to share my story, so take a peek!";
+    }
     
-
-
-
-public String Name, Color, Breed;
-public int Age;
-
-
-private static int  NINE = 9;
-static public  int numberoflives= NINE;
-
-public cat(String meow,
-final String c,final int age,
-final String korat) {
-    this.Name =meow ;
-    Color=c  ;
-    this.Age=age;
-    Breed= korat;
-}
-
-// This method is useful because it allows the cat to meow and introduce itself to the world in a single line of code.
-    public String
-    Meow()
-    {return "Meow, meow, I am " + Name + ", hear me purr, In this feline world, I'm the star, that's for sure. With whiskers long and fur so sleek, I'm here to share my story, so take a peek!";
-}public
-
-
-
-void setcolor(String c) {
-    this.Color = 
-    c;
-}
-
-    public String GET_COLOR() {
-return this.Color;
+    public void setColor(String c) {
+        this.color = c;
     }
 
-    public int hasSameColor( cat  cat )
-    {
-if (Color==cat.Color) {return   1;}else{return 0;}
+    public String getColor() {
+        return this.color;
     }
 
-String name( )
-{
-    return Name;
-}
-
-
-String BREED( )
-
-{
-    return 
-    this.Breed;
-}
-
-public
-static
-void
-main(
-    String[] ARGS
-) {
-    cat cat = new cat(
-        "Kiri",
-        "Gray",
-        4, "Kor"+
-         "at");
-
-    System.
-    out.
-    println(cat.Meow());
-}
+    public int hasSameColor(Cat cat){
+        if (this.getColor()==cat.getColor()) {
+            return  1;
+        } else {
+            return 0;
         }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBreed() {
+        return this.breed;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Kiri", "Gray",4, "Korat");
+        System.out.println(cat.meow());
+    }
+}
