c2bb4c3f8f5de782fbf31f129da1c77c3ebbbc58
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-17 10:44:45 +0200

    Fix SmartLamp implementation

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
@@ -1,29 +1,32 @@
 package it.unibo.encapsulation;
 
 public class SmartLamp {
-    private boolean status;
+    private boolean isOn;
     private int intensity;
     private String color;
 
-    public SmartLamp(boolean status, int intensity, String color) {
-        this.status = status;
+    public SmartLamp(boolean isOn, int intensity, String color) {
+        this.isOn = isOn;
         this.intensity = intensity;
         this.color = color;
     }
-    public boolean isStatus() {
-        return this.status;
+
+    public boolean isOn() {
+        return this.isOn;
     }
+
     public void changeStatus() {
-        this.status = !this.status;
+        this.isOn = !this.isOn;
     }
+
     public int getIntensity() {
         return this.intensity;
     }
+
+    //Se la lampadina è spenta viene accesa quando si setta l'intensità
     public void setIntensity(int intensity) {
-        if(intensity != 1 && intensity!=2 && intensity!=3){
-            System.out.println("Valore di intensità non valido");
-        }else{
-            if(!this.status){
+        if(intensity == 1 && intensity==2 && intensity==3){
+            if(!this.isOn) {
                 changeStatus();
             }
             this.intensity = intensity;
@@ -33,12 +36,11 @@ public class SmartLamp {
     public String getColor() {
         return this.color;
     }
-    
+
+    //Se la lampadina è spenta viene accesa quando si setta il colore
     public void setColor(String color) {
-        if(color != "bianco" && color!="rosso" && color!="blu" && color!="verde"){
-            System.out.println("Colore non valido");
-        }else{
-            if(!this.status){
+        if(color == "bianco" && color=="rosso" && color=="blu" && color=="verde"){
+            if(!this.isOn){
                 changeStatus();
             }
             this.color = color;
