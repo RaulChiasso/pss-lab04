09d36ebb2ceaee82f483756391f01d926838e75b
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-17 10:14:51 +0200

    Add Test for Calculator.java in TestAllClasses

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/TestAllClasses.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/TestAllClasses.java
@@ -12,6 +12,15 @@ public class TestAllClasses {
         System.out.println("8/4=" + calc.div(8, 4));
         Calculator.printCalculatorStatus(calc);
 
+        System.out.println("----- TEST GETTERS AND SETTERS CALCULATOR -----");
+        System.out.println(calc.getLastResult());
+        System.out.println(calc.getOperationsPerformed());
+        calc.setLastResult(0);
+        calc.setOperationsPerformed(0);
+        System.out.println("Settato tutto a 0 con i Setters");
+        System.out.println(calc.getLastResult());
+        System.out.println(calc.getOperationsPerformed());
+
         final Smartphone htcOne = new Smartphone("HTC", "One", 1024);
         final Smartphone note3 = new Smartphone(4, 2048, 8192, "Samsung", "Galaxy Note 3", true, true, true);
         htcOne.printStringRep();
