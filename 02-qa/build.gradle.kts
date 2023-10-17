7901b5a94fe08ac43c4b5acbffd51ecba66cb148
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-17 11:30:06 +0200

    Add plugins in build.gradle.kts

--- 02-qa/build.gradle.kts
+++ 02-qa/build.gradle.kts
@@ -1,1 +1,10 @@
-plugins { java }
+plugins {
+  java // Carica il necessario per la compilazione di Java
+  application // Carica il necessario per l'esecuzione di classi con la JVM
+  checkstyle
+}
+repositories { mavenCentral() } // Configura Gradle per cercare e scaricare da Maven Central
+
+application { // Configurazione dell'esecuzione, la main class è sufficiente
+    mainClass.set("it.unibo.qa.Cat")
+}
